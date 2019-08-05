dac6864795b7c83d4c7ff3ee106e7822b8fc6383
tonylo1128 <tonylo19961128@gmail.com>
2019-08-05 21:54:53 +0800

    error fixing

--- src/component/ButtonColor.js
+++ src/component/ButtonColor.js
@@ -5,7 +5,7 @@ import * as actionFromColorAction from '../Action/colorAction/colorAction'
 function ButtonColor({colorFromState, colorChangeFun}){
     return(
         <div>
-            <h1 color={colorFromState}>Testing!</h1>
+            <h1 style={{ color:colorFromState} }> Testing!</h1>
             <button onClick={()=>colorChangeFun()} >Primary</button>
             <h1>{colorFromState}</h1>
             {console.log("The state is:", colorFromState)}
